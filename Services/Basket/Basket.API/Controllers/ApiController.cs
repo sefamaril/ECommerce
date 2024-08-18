d1593a3dca9e6438f1e065f3c449821047203ce5
sefamaril <sefamaril@hotmail.com>
2024-08-18 19:32:04 +0300

    Implemented BasketController with MediatR and API versioning.
    Added BasketController with methods for getting, updating, and deleting baskets.
    Integrated MediatR for handling commands and queries.
    Configured API versioning with Asp.Versioning

---
+++ Services/Basket/Basket.API/Controllers/ApiController.cs
@@ -0,0 +1,12 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[Controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+    }
+}
