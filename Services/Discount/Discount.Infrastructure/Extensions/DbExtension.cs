9d4e29d2d7b6f04bb6606b92c272089ea350de9e
sefamaril <sefamaril@hotmail.com>
2024-08-25 23:26:06 +0300

    Implemented Repositories and added Extension methods for database migration and initialization.

---
+++ Services/Discount/Discount.Infrastructure/Extensions/DbExtension.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+
+namespace Discount.Infrastructure.Extensions
+{
+    public static class DbExtension
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var config = services.GetRequiredService<IConfiguration>();
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+                try
+                {
+                    logger.LogInformation("Discount DB Migration Started");
+                    ApplyMigrations(config);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    throw;
+                }
+            }
+            return host;
+        }
+
+        private static void ApplyMigrations(IConfiguration config)
+        {
+            using var connection = new NpgsqlConnection(config.GetValue<string>("DatabaseSettings:ConnectionString"));
+            connection.Open();
+            using var cmd = new NpgsqlCommand()
+            {
+                Connection = connection,
+            };
+            cmd.CommandText = "DROP TABLE IF EXISTS Coupon";
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY,
+                                                    ProductName VARCHAR(500) NOT NULL,
+                                                    Description TEXT,
+                                                    Amount INT)";
+
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('iPhone 13', 'iPhone Discount', 500)";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('MacBook Pro', 'Macbook Discount', 700)";
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
+
+// Author: Sefa Maril - Date: 25.08.2024
+// Description: Extension methods for database migration and initialization.
