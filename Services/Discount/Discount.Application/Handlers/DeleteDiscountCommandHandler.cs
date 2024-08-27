fbaf602247fdf01fce83ec00b32f95c63a1e8449
sefamaril <sefamaril@hotmail.com>
2024-08-28 00:20:30 +0300

    Add Discount Command and Query Handlers with gRPC Proto Definitions

---
+++ Services/Discount/Discount.Application/Handlers/DeleteDiscountCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Discount.Application.Commands;
+using Discount.Core.Entities;
+using Discount.Core.Repositories;
+using Discount.Grpc.Protos;
+using MediatR;
+
+namespace Discount.Application.Handlers
+{
+    // Author: Sefa Maril - Date: 28.08.2024
+    // Description: Handles the deletion of a discount.
+    public class DeleteDiscountCommandHandler : IRequestHandler<DeleteDiscountCommand, bool>
+    {
+        private readonly IDiscountRepository _discountRepository;
+
+        public DeleteDiscountCommandHandler(IDiscountRepository discountRepository)
+        {
+            _discountRepository = discountRepository;
+        }
+        public async Task<bool> Handle(DeleteDiscountCommand request, CancellationToken cancellationToken)
+        {
+            var deletedDiscount = await _discountRepository.DeleteDiscount(request.ProductName);
+            return deletedDiscount;
+        }
+    }
+}
