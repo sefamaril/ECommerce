c93c1f312b1e0e988a0bc72b8072f05199862b88
sefamaril <sefamaril@hotmail.com>
2024-09-19 23:46:53 +0300

    Add OrderRepository and AsyncRepository interfaces

---
+++ Services/Ordering/Ordering.Core/Repositories/IAsyncRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Ordering.Core.Repositories;
+
+public interface IAsyncRepository<T>
+{
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task<IReadOnlyList<T>> GetAllAsync(Expression<Func<T, bool>> predicate);
+    Task<T> GetByIdAsync(int id);
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+}
