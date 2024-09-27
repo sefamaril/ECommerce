59e12d37c44284bfd7d4dafb4f1a25bdaeab95c4
sefamaril <sefamaril@hotmail.com>
2024-09-27 17:01:00 +0300

    Add OrderNotFoundException, OrderMapping Profile, GetOrderListQuery, OrderRepository and command classes with handlers

---
+++ Services/Ordering/Ordering.Application/Handlers/CheckOutOrderCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Commands;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+
+namespace Ordering.Application.Handlers;
+
+public class CheckOutOrderCommandHandler : IRequestHandler<CheckOutOrderCommand, int>
+{
+    private readonly IOrderRepository _orderRepository;
+    private readonly IMapper _mapper;
+    private readonly ILogger _logger;
+
+    public CheckOutOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger logger)
+    {
+        _orderRepository = orderRepository;
+        _mapper = mapper;
+        _logger = logger;
+    }
+
+    public async Task<int> Handle(CheckOutOrderCommand request, CancellationToken cancellationToken)
+    {
+        var orderEntity = _mapper.Map<Order>(request);
+        var generateOrder = await _orderRepository.AddAsync(orderEntity);
+        _logger.LogInformation($"Order {generateOrder} successfully created");
+        return generateOrder.Id;
+    }
+}
