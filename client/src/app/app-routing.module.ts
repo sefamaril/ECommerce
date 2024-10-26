826c5b6d5a9cbaf8547bff301d0bc2a650f2e741
sefamaril <sefamaril@hotmail.com>
2024-10-26 14:07:47 +0300

    Add error handling components and update routing
    - Fix lazy loading issue
    - Added NotFoundComponent for 404 errors
    - Added ServerErrorComponent for 500 errors
    - Added UnAuthenticatedComponent for 401 errors
    - Updated app-routing.module.ts to include new routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { UnAuthenticatedComponent } from './core/un-authenticated/un-authenticated.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: 'server-error', component: ServerErrorComponent },
+  { path: 'un-authenticated', component: UnAuthenticatedComponent },
   { path: 'store', loadChildren: () => import('./store/store.module').then(mod => mod.StoreModule) },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { UnAuthenticatedComponent } from './un-authenticated/un-authenticated.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [
+    NavbarComponent,
+    NotFoundComponent,
+    UnAuthenticatedComponent,
+    ServerErrorComponent],
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports:[
+  exports: [
     NavbarComponent
   ]
 })
